3364d0cff20b2da4ed75eb4a4569f3149a95881c
olast <olastephennet@gmail.com>
2024-05-30 21:12:57 +0100

    updates on the authentications

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'attendees',
+        title: 'Attendees',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/contacts'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'attendees',
+        title: 'Attendees',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/contacts'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'attendees',
+        title: 'Attendees',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/contacts'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'attendees',
+        title: 'Attendees',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/contacts'
+    }
+];
---
+++ src/app/modules/auth/confirm-email/confirm-email.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AuthConfirmEmailComponent } from 'app/modules/auth/confirm-email/confirm-email.component';
+
+export default [
+    {
+        path: '',
+        component: AuthConfirmEmailComponent,
+    },
+] as Routes;
+
